47a03df85a4e54f721c7ccfcf9e293dea259bd47
jeremyyonathan <jeremywib7@gmail.com>
2023-04-13 11:21:15 +0700

    Initial commit

---
+++ src/main/java/com/example/springjakartamaven/HelloRequest.java
@@ -0,0 +1,9 @@
+package com.example.springjakartavalidation;
+
+import lombok.Data;
+
+@Data
+public class HelloRequest {
+    private String firstName;
+    private String lastName;
+}
---
+++ src/main/java/com/example/springjakartamaven/SpringJakartaMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjakartamaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJakartaMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJakartaMavenApplication.class, args);
+	}
+
+}
